c8435c078876f6cbe540a2b24e8841bb3b196c24
marr9880 <229481176+marr9880@users.noreply.github.com>
2026-06-27 10:39:49 -0500

    Selects correct list and tasks now. Checkboxes on tasks function properly. Need to write code for delete buttons.

--- index.js
+++ index.js
@@ -1,63 +1,91 @@
 const toDoListContainer = document.querySelector(".to-do-list");
-const newToDoForm = document.querySelector(".new-to-do-form");
-const newToDoInput = document.querySelector(".new-to-do-input");
 const tasksListContainer = document.querySelector(".tasks-list");
-const newTaskForm = document.querySelector(".new-task-form");
+const newToDoInput = document.querySelector(".new-to-do-input");
+const newToDoDueDate = document.querySelector(".new-to-do-due-date");
 const newTaskInput = document.querySelector(".new-task-input");
+const newToDoForm = document.querySelector(".new-to-do-form");
+const newTaskForm = document.querySelector(".new-task-form");
 
 const LOCAL_STORAGE_TODO_KEY = "todo.lists";
 const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = "todo.selectedListId";
 
 let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_TODO_KEY)) || [];
-let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY);
+let selectedListId;// = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY);
 
 toDoListContainer.addEventListener("click", e => {
     if (e.target.tagName.toLowerCase() === "li") {
-        selectedListId = e.target.dataset.listID;
+        selectedListId = e.target.id;
         renderFunctions.renderToDoList();
+        renderFunctions.renderTaskList();
     }
 });
-
-
 
 function save() {
     localStorage.setItem(LOCAL_STORAGE_TODO_KEY, JSON.stringify(lists));
     localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId);
 };
 
-
-
 const renderFunctions = (() => {
+   
     const renderToDoList = () => {
         toDoListContainer.replaceChildren();
         lists.forEach(list => {
             const toDoListElement = document.createElement("li");
-            toDoListElement.dataset.listID = list.id;
+            toDoListElement.id = list.id;
             toDoListElement.classList.add("to-do");
-            toDoListElement.innerText = list.name;
+            toDoListElement.textContent = `${list.name} due by ${list.dueDate}`;
             if (list.id === selectedListId) {
-                toDoListElement.classList.add("active")
-            }    
+                toDoListElement.classList.add("active");
+            }
+            const today = new Date().toISOString().slice(0, 10);
+            if (list.dueDate < today) {
+                toDoListElement.style.backgroundColor = "rgba(202, 102, 102, 0.65)";
+            } else if (list.dueDate > today) {
+                toDoListElement.style.backgroundColor = "rgb(95, 201, 95)";
+            } else {
+                toDoListElement.style.backgroundColor = "rgb(230, 230, 99)";
+            }
             toDoListContainer.appendChild(toDoListElement);
         })
     };
 
     const renderTaskList = () => {
         tasksListContainer.replaceChildren();
-        const targetList = lists.find(list => list.listID = selectedListId);
+        let targetList = lists.find(list => list.id === selectedListId);
+        if (targetList == null) {
+            alert("You must select a To-Do!");
+            return;
+        }
         let tasks = targetList.tasks;
         tasks.forEach(task => {
             const taskListElement = document.createElement("li");
             const taskLabel = document.createElement("label");
-            taskLabel.innerText = task.name;
+            taskLabel.classList.add("not-checked");
+            taskLabel.textContent = task.name;
+            taskLabel.id = task.id;
             const taskInput = document.createElement("input");
             taskInput.name = task.name;
             taskInput.value = task.name;
+            taskInput.id = task.id;
             taskInput.type = "checkbox";
             taskListElement.appendChild(taskInput);
             taskListElement.appendChild(taskLabel);
             tasksListContainer.appendChild(taskListElement);
-        })
+            
+            taskInput.addEventListener("change", e => {
+                if (e.target.checked) {
+                    task.isComplete = true;
+                    if (taskInput.id === taskLabel.id) {
+                        taskLabel.classList.replace("not-checked", "checked");
+                    }
+                } else {
+                    task.isComplete = false;
+                    if (taskInput.id === taskLabel.id) {
+                        taskLabel.classList.replace("checked", "not-checked");
+                    }
+                }
+            });
+         });
     };
 
 
@@ -66,15 +94,18 @@ const renderFunctions = (() => {
 }) ();
 
 const clickAndSubmitFunctions = (() => {
+    
     const newToDoFormSubmit = (event) => {
         event.preventDefault();
         const toDoName = newToDoInput.value;
-        if (toDoName == null || toDoName === "") return;
-        function createToDo(name) {
-            return { id: crypto.randomUUID(), name: name, tasks: [] };
+        const toDoDueDate = newToDoDueDate.value;
+        if (toDoName == null || toDoName === "" || toDoDueDate == null || toDoDueDate === "") return;
+        function createToDo(name, date) {
+            return { id: crypto.randomUUID(), name: name, dueDate: date, tasks: [], isComplete: false };
         };
-        const toDo = createToDo(toDoName);
+        const toDo = createToDo(toDoName, toDoDueDate);
         newToDoInput.value = null;
+        newToDoDueDate.value = null;
         lists.push(toDo);
         renderFunctions.renderToDoList();
     };
@@ -84,11 +115,11 @@ const clickAndSubmitFunctions = (() => {
         const taskName = newTaskInput.value;
         if (taskName == null || taskName === "") return;
         function createTask(name) {
-            return { id: crypto.randomUUID(), name: name};
+            return { id: crypto.randomUUID(), name: name, isComplete: false };
         };
         const task = createTask(taskName);
         newTaskInput.value = null;
-        const targetList = lists.find(list => list.listID = selectedListId);
+        let targetList = lists.find(list => list.id === selectedListId);
         if (targetList) {
             targetList.tasks.push(task);
         }
